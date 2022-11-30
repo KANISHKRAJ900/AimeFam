cc9e3208e56e9b44beb892a199984e1cee106b24
@kanishk900 <walkerrahil@gmail.com>
2022-11-30 17:55:15 +0530

    a

--- src/Components/Cards/RecommandedCard.jsx
+++ src/Components/Cards/RecommandedCard.jsx
@@ -4,8 +4,9 @@ const RecommandedCard = () => {
   return (
     <>
       <div className="lg:px-16 sm:px-4 flex flex-col sm:flex-row      sm:py-8  justify-center gap-8">
-        <article className="w-80 px-2 hover:bg-[url(https://demonslayer-hinokami.sega.com/img/purchase/digital-standard.jpg)] bg-cover bg-no-repeat bg-center   rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:shadow-sm mx-auto">
-          <div className="rounded-[10px]  p-4 !pt-20 sm:p-6 ">
+        <article className="w-80 px-2 shadow-lg hover:bg-[url(https://demonslayer-hinokami.sega.com/img/purchase/digital-standard.jpg)] bg-cover bg-no-repeat bg-center   rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:shadow-sm mx-auto relative">
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70 rounded-xl z-10"></div>
+          <div className="rounded-[10px] relative z-[9999] p-4 !pt-20 sm:p-6">
             <time
               datetime="2022-10-10"
               className="block text-sm animate-bounce text-white"
@@ -14,7 +15,7 @@ const RecommandedCard = () => {
             </time>
 
             <a href="#">
-              <h3 className="mt-0.5  text-lg font-medium text-gray-900">
+              <h3 className="mt-0.5  text-lg font-medium text-white">
                 How to center an element using JavaScript and jQuery
               </h3>
             </a>
