b92660cd34c2966e89f9b3a4778026c93deb5da4
@kanishk900 <walkerrahil@gmail.com>
2022-12-04 23:55:18 +0530

    performance

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
       <footer aria-label="Site Footer" className="bg-gray-50">
         <div className="mx-auto max-w-screen-xl px-4 py-2 sm:px-6 lg:px-8">
           <div className="sm:flex sm:items-center sm:justify-between">
-            <div className="flex justify-center text-teal-600 sm:justify-start">
+            <div className="flex justify-center text-teal-900 sm:justify-start">
               <p className="text-3xl">
                 Anime <span className="text-sm">Season</span>
               </p>
             </div>
 
-            <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right cursor-pointer">
+            <p className="mt-4 text-center text-sm text-gray-400 lg:mt-0 lg:text-right cursor-pointer">
               <a
                 href="https://www.privacypolicygenerator.info/live.php?token=KTmj8HxpFMIdSTA7fgIyBog2nsVoOjqC"
                 target="_blank"
