bdf3d7ece5f3c58c89b67c9f0b38f9a5b8af8e71
@kanishk900 <walkerrahil@gmail.com>
2022-12-04 17:50:56 +0530

    ga

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import ReactGa from "react-ga";
 
 function App() {
+  const TRACKING_ID = "G-ZB1Y218Q5D";
+  ReactGa.initialize(TRACKING_ID);
   return (
     <>
       <BrowserRouter>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import About from "./Components/About/About";
 import AnimeCard from "./Components/Cards/AnimeCard";
 import RecommandedCard from "./Components/Cards/RecommandedCard";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import ReactGa from "react-ga";
 
 const Home = () => {
+  useEffect(() => {
+    ReactGa.pageview(window.location.pathname);
+  }, []);
+
   return (
     <div className="text-lg font-medium text-red-900">
       <Header />
